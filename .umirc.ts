5709b79f5b02479471dbde68257a2ab1ef9eaf93
GYBSL <1290005228@qq.com>
2022-09-12 02:28:19 +0800

    修改配置文件

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: '开发文档',
+  title: 'React文档',
   description: '一些小问题会更新到文档中',
   favicon: 'https://portrait.gitee.com/uploads/avatars/user/3440/10321374_gybsl_1641986311.png',
   logo: 'https://portrait.gitee.com/uploads/avatars/user/3440/10321374_gybsl_1641986311.png',
   outputPath: 'docs-dist',
   mode: 'doc',
+  hash: true,
+  history: {
+    type: 'hash',
+  },
+  base: '/',
+  publicPath: '/',
   exportStatic: {},
 });
