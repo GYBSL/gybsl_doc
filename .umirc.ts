1d3d0ce8d44c1733ec0b01ad5138487d4e77b79a
GYBSL <1290005228@qq.com>
2022-09-12 02:35:58 +0800

    修改了配置文件标题

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'React文档',
-  description: '一些小问题会更新到文档中',
+  description: 'React学习文档',
   favicon: 'https://portrait.gitee.com/uploads/avatars/user/3440/10321374_gybsl_1641986311.png',
   logo: 'https://portrait.gitee.com/uploads/avatars/user/3440/10321374_gybsl_1641986311.png',
   outputPath: 'docs-dist',
